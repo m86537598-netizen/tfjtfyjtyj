c2a1da3c684facd704f25979e18d36fd5f9fbd7f
Pet-story <m86537598@gmail.com>
2025-12-26 04:09:32 -0500

    Update Secondary.lua

--- Secondary.lua
+++ Secondary.lua
@@ -2,4 +2,4 @@
 
 local mainload = "https://raw.githubusercontent.com/m86537598-netizen/tfjtfyjtyj/refs/heads/main/Main.lua"
 
-loadstring(game:HttpGet(mainload))
+loadstring(game:HttpGet(mainload))()
